97d25a97e687b4c94ff8d7bd3e2035380865cc8f
manzar96 <manoszar96@gmail.com>
2021-01-25 15:29:27 +0200

    fixed bugs on frontend

--- frontend/src/js/page1.js
+++ frontend/src/js/page1.js
@@ -1,6 +1,5 @@
 function makerequest_async () {
     let xmlhttp = new XMLHttpRequest()
-
     xmlhttp.open("GET", "http://localhost:5001/page1", true);
     xmlhttp.send();
     xmlhttp.onreadystatechange = function () {
@@ -36,7 +35,7 @@ function submitform(event){
             }
         }
         sessionStorage.setItem("conversations_page1_results", JSON.stringify(results_page1));
-        window.location.replace("../html/page2.html");
+        window.location.href="../html/page2.html";
 
      }
     else{
@@ -56,28 +55,29 @@ const getFormJSON = (form) => {
 
 function writeconversations() {
     json_text = sessionStorage.getItem("conversations_page1");
-    document.getElementById("add1").innerHTML = ""
+    if (json_text != null){
     dict = JSON.parse(json_text)
     counter = 1
+    mystring = ""
     for (key in dict){
         if(dict.hasOwnProperty(key)){
-            document.write("<br><b>Conversation ")
-            document.write(counter+"</b><br>")
+            mystring +=("<br><b>Conversation ")
+            mystring +=(counter+"</b><br>")
             input = dict[key]["input"]
             mymodel = dict[key]["mymodel"]
 
             if (input.length === 1){
-                document.write("<br><div style=\"color:green;\">Speaker:</div>")
-                document.write(input[0])
-                document.write("<br>")
-                document.write("<br><div" +
+                mystring +=("<br><div style=\"color:green;\">Speaker:</div>")
+                mystring +=(input[0])
+                mystring +=("<br>")
+                mystring +=("<br><div" +
                     " style=\"color:blue;\">Generated Response:</div>")
-                document.write(mymodel)
-                document.write("<br>")
-                document.write("<br>")
-                document.write("Rate Empathy: ")
-                document.write("<form>")
-                document.write("<select id=emp"+counter+" class=page1" +
+                mystring +=(mymodel)
+                mystring +=("<br>")
+                mystring +=("<br>")
+                mystring +=("Rate Empathy: ")
+                mystring +=("<form>")
+                mystring +=("<select id=emp"+counter+" class=page1" +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -86,11 +86,11 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
-                document.write("Rate Relevance: ")
-                document.write("<form>")
-                document.write("<select id=rel"+counter+" class=page1 " +
+                mystring +=("</form>")
+                mystring +=("<br>")
+                mystring +=("Rate Relevance: ")
+                mystring +=("<form>")
+                mystring +=("<select id=rel"+counter+" class=page1 " +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -99,11 +99,11 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
-                document.write("Rate Fluency: ")
-                document.write("<form>")
-                document.write("<select id=flu"+counter+" class=page1 " +
+                mystring +=("</form>")
+                mystring +=("<br>")
+                mystring +=("Rate Fluency: ")
+                mystring +=("<form>")
+                mystring +=("<select id=flu"+counter+" class=page1 " +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -112,29 +112,29 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
+                mystring +=("</form>")
+                mystring +=("<br>")
 
             }
             else{
-                document.write("<br><div style=\"color:green;\">Speaker:</div>")
-                document.write(input[0])
-                document.write("<br>")
-                document.write("<br><div" +
+                mystring +=("<br><div style=\"color:green;\">Speaker:</div>")
+                mystring +=(input[0])
+                mystring +=("<br>")
+                mystring +=("<br><div" +
                     " style=\"color:blueviolet;\">Listener:</div>")
-                document.write(input[1])
-                document.write("<br>")
-                document.write("<br><div style=\"color:green;\">Speaker:</div>")
-                document.write(input[2])
-                document.write("<br>")
-                                document.write("<br><div" +
+                mystring +=(input[1])
+                mystring +=("<br>")
+                mystring +=("<br><div style=\"color:green;\">Speaker:</div>")
+                mystring +=(input[2])
+                mystring +=("<br>")
+                                mystring +=("<br><div" +
                     " style=\"color:blue;\">Generated Response:</div>")
-                document.write(mymodel)
-                document.write("<br>")
-                document.write("<br>")
-                document.write("Rate Empathy: ")
-                document.write("<form>")
-                document.write("<select id=emp"+counter+" class=page1 " +
+                mystring +=(mymodel)
+                mystring +=("<br>")
+                mystring +=("<br>")
+                mystring +=("Rate Empathy: ")
+                mystring +=("<form>")
+                mystring +=("<select id=emp"+counter+" class=page1 " +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -143,11 +143,11 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
-                document.write("Rate Relevance: ")
-                document.write("<form>")
-                document.write("<select id=rel"+counter+" class=page1 " +
+                mystring +=("</form>")
+                mystring +=("<br>")
+                mystring +=("Rate Relevance: ")
+                mystring +=("<form>")
+                mystring +=("<select id=rel"+counter+" class=page1 " +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -156,11 +156,11 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
-                document.write("Rate Fluency: ")
-                document.write("<form>")
-                document.write("<select id=flu"+counter+" class=page1 " +
+                mystring +=("</form>")
+                mystring +=("<br>")
+                mystring +=("Rate Fluency: ")
+                mystring +=("<form>")
+                mystring +=("<select id=flu"+counter+" class=page1 " +
                     " required>\n" +
                     "   <option disabled selected value> -- select an option -- </option>" +
                     "   <option value=\"1\">1</option>" +
@@ -169,13 +169,15 @@ function writeconversations() {
                     "   <option value=\"4\">4</option>" +
                     "   <option value=\"5\">5</option>  " +
                     " </select>")
-                document.write("</form>")
-                document.write("<br>")
+                mystring +=("</form>")
+                mystring +=("<br>")
             }
         }
         counter +=1
-
     }
+    }
+    document.getElementById("add1").innerHTML = mystring
+
 }
 
 
